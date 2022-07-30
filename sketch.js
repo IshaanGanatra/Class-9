68bbac7aed0b9dd1b2fd5279c59a66c4df205fe7
IshaanGanatra <110284709+IshaanGanatra@users.noreply.github.com>
2022-07-30 11:08:10 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var box;
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200,200,30,30);
+}
+
+function draw() 
+{background(30);
+  if (keyIsDown(LEFT_ARROW)){
+box.position.x = box.position.x -5;
+}
+  if (keyIsDown(RIGHT_ARROW)){
+box.position.x = box.position.x +5;
+ }
+ if (keyIsDown(UP_ARROW)){
+  box.position.y = box.position.y -5;
+  }
+  if (keyIsDown(DOWN_ARROW)){
+  box.position.y = box.position.y+5;
+   }
+  drawSprites();
+}
